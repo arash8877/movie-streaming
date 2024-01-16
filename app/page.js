64381f2f4118660a62fcc802aaaf3d1beb60b4f1
Arash <arash8877@gmail.com>
2024-01-16 19:26:00 +0100

    modified app/page.js

--- app/page.js
+++ app/page.js
@@ -1,9 +1,32 @@
-export default function Home() {
+import Movie from "./Movie";
+
+export default async function Home() {
+  const data = await fetch(
+    `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`
+  );
+  const res = await data.json();
   return (
-    <main >
-    <main >
-        <h1>this is the title</h1>
+    <main>
+      <main>
+        <div className="grid gap-16 grid-cols-fluid">
+          {res.results.map((movie) => (
+            <Movie
+              key={movie.id}
+              id={movie.id}
+              title={movie.original_title}
+              poster_path={movie.poster_path}
+              release_date= {movie.release_date}
+              rate={movie.vote_count}
+            />
+          ))}
+        </div>
+      </main>
     </main>
-    </main>
-  )
+  );
 }
+
+//in Next.js all components are 'server-side' component by default.
+// this means all hooks like useState, useEffect,... also events like
+// onClick,... don't work. You need to make a 'client-side' component
+// to use hooks and events.
+//I made Movie.jsx as a client-side component and imported here.

b8f38a2e78eb08e72e43254c5debe0f173f9098d
Arash <arash8877@gmail.com>
2024-01-17 11:17:01 +0100

    modified next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,22 @@
-module.exports = {
+
+// @type { import ('next').Next.Config} 
+
+const nextConfig = {
+  experimental: {
+    appDir: true, 
+  },
   images: {
     domains: ["image.tmdb.org"],
-  },
-};
+  }
+}
+
+
+
+
+// module.exports = {
+//   images: {
+//     domains: ["image.tmdb.org"],
+//   },
+// };
 
 
